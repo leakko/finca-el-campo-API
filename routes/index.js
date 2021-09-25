c5597d18731979dcb14ca84f42b9d3639e153b0a
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-25 20:27:46 +0200

    login route implemented

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const authMiddleware = require("../middlewares/auth.middleware");
+const authController = require("../controllers/auth.controller")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+//Auth
+router.post("/login", authMiddleware.isNotAuthenticated, authController.login)
 
 module.exports = router;
