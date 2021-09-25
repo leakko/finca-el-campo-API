cbfbe3344df126ee8f28a63d5c1ba77a4eabfec6
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-25 13:20:30 +0200

    User model done

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,54 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const SALT_ROUNDS = 10;
+
+function validateEmail(email) {
+  const re = EMAIL_PATTERN;
+  return re.test(String(email).toLowerCase());
+}
+
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: [true, 'Email address is required'],
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [EMAIL_PATTERN, 'Please fill a valid email address']
   },
-  password: String,
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password is too short"]
+  },
+  role: {
+    type: String,
+    enum: ["USER, ADMIN"],
+    default: "USER"
+  }
+},
+{
+  timestamps: true
 });
+
+userSchema.pre("save", function(next) {
+  if (this.isModified("password")) {
+    bcrypt.hash(this.password, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+  } else {
+    next();
+  }
+})
+
+userSchema.methods.checkPassword = function(passwordToCheck) {
+  return bcrypt.compare(passwordToCheck, this.password);
+}
 
 const User = model("User", userSchema);
 
