317dd91a462abcdafb0524026aded3a3c8eebf94
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-26 11:29:51 +0200

    Login and register routes created

---
+++ controllers/users.controller.js
@@ -0,0 +1,13 @@
+const User = require("../models/User.model");
+
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.currentUser)
+    .then((user) => res.json(user))
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  User.create(req.body)
+    .then((user) => res.json(user))
+    .catch(next);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 const authController = require("../controllers/auth.controller")
+const usersController = require("../controllers/users.controller")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-//Auth
+/* AUTH */
+
+//login
 router.post("/login", authMiddleware.isNotAuthenticated, authController.login)
+
+//register
+router.post(
+  "/users",
+  authMiddleware.isNotAuthenticated,
+  usersController.createUser
+);
 
 module.exports = router;
