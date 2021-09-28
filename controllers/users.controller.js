bf95236f65e93d75a6ab7de40cfe9c1c60230993
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-28 11:44:11 +0200

    CreateUser controller updated to send more feedback to front

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -9,5 +9,11 @@ module.exports.getCurrentUser = (req, res, next) => {
 module.exports.createUser = (req, res, next) => {
   User.create(req.body)
     .then((user) => res.json(user))
-    .catch((error) => res.json({"errorMessage": error.errors.password ? error.errors.password.properties.message : error.errors.email.properties.message}));
+    .catch((error) => {
+      if(error.code === 11000) {
+        res.json({"errorMessage": "El email ya está en uso, elige otro"})
+      } else {
+        res.json({"errorMessage": error.errors.password ? error.errors.password.properties.message : error.errors.email.properties.message})
+      }
+  });
 };
