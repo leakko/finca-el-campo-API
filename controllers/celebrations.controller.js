f745364ba1c4d62f3206c53d4d8cf2f0d82a955c
Marcos Salvo <marcossalvo95@gmail.com>
2021-10-02 10:01:58 +0200

    Celebrations routes implemented

---
+++ controllers/celebrations.controller.js
@@ -0,0 +1,19 @@
+const Celebration = require("../models/Celebration.model")
+
+module.exports.getCelebrations = (req, res, next) => {
+    Celebration.find()
+    .then((celebrations) => res.json(celebrations))
+    .catch(next);
+};
+
+module.exports.createCelebration = (req, res, next) => {
+    Celebration.create(req.body)
+    .then((createdCelebration) => res.json(createdCelebration))
+    .catch((error) => {
+        if( error.errors.date) {
+            res.json({"errorMessage": error.errors.date.properties.message})
+        } else {
+            res.json({"errorMessage": error.errors.client.properties.message})
+        }
+    });
+};
